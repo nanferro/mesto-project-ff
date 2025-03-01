afd6cc1be98bb40d45a291c96d0ba401da3bc4f7
nanferro <nalavrova97@gmail.com>
2025-03-01 17:32:00 +0200

    final version

---
+++ src/scripts/card.js
@@ -0,0 +1,27 @@
+export function createCard(cardData, deleteCallback, likeCallback, imageCallback) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.cloneNode(true).querySelector(".card");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  
+  cardImage.src = cardData.link;
+  cardTitle.textContent = cardData.name;
+  cardImage.alt = cardData.name;
+  
+  deleteButton.addEventListener("click", () => deleteCallback(cardElement));
+  likeButton.addEventListener("click", likeCallback);
+  cardImage.addEventListener("click", imageCallback);
+  
+  return cardElement;
+}
+
+export function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+export function toggleLike(event) {
+  event.target.classList.toggle("card__like-button_is-active");
+}
+  
---
+++ src/scripts/modal.js
@@ -0,0 +1,26 @@
+export function openModal(popup) {
+  popup.classList.add('popup_is-opened', 'popup_is-animated');
+  document.addEventListener('keydown', closeOnEsc);
+}
+  
+export function closeModal(popup) {
+  popup.classList.remove('popup_is-opened', 'popup_is-animated');
+  document.removeEventListener('keydown', closeOnEsc);
+}
+
+function closeOnEsc(event) {
+  if (event.key === 'Escape') {
+    const openPopup = document.querySelector('.popup_is-opened');
+    if (openPopup) {
+      closeModal(openPopup);
+    }
+  }
+}
+  
+export function closeModalOnOverlay(popup) {
+  popup.addEventListener('click', (event) => {
+    if (event.target === popup) {
+      closeModal(popup);
+    }
+  });
+}
